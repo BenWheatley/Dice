d75271cfa5c2ca6cd1757be4b62bff18d1ede37b
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-09-27 23:16:55 +0200

    Initial Commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Ben Wheatley on 2018/09/27.
+//  Copyright © 2018 Ben Wheatley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
