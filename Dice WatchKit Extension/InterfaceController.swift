1f26f5ed3be77f5090dd906affc0537e962d809b
Ben Wheatley <BenWheatley@users.noreply.github.com>
2018-09-27 23:46:01 +0200

    Most basic 'dice' watch app possible. Shows dice on watch. Tap dice to re-roll. Just a d6, no options, no animations.

--- Dice WatchKit Extension/InterfaceController.swift
+++ Dice WatchKit Extension/InterfaceController.swift
@@ -11,14 +11,16 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+	
+	@IBOutlet weak var diceButton: WKInterfaceButton!
+	
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
-    
-    override func willActivate() {
+	
+	override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
@@ -27,5 +29,11 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+	
+	@IBAction func roll() {
+		let dice = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+		let roll = Int(arc4random_uniform(UInt32(dice.count)))
+		diceButton.setTitle(dice[roll])
+	}
 
 }
